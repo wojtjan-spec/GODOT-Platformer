b88c83557de2ae4eba8256c66a2d59445417a5d3
Jan Wojtkowski <janwojtkowski@Jans-MacBook-Pro.local>
2025-02-04 20:24:30 +0100

    Add start menu

---
+++ start_menu.gd
@@ -0,0 +1,18 @@
+extends CenterContainer
+
+@onready var start_game_button: Button = %StartGameButton
+@onready var quit_game_button: Button = %QuitGameButton
+
+func _ready():
+	start_game_button.grab_focus()
+	pass
+
+
+func _on_start_game_button_pressed() -> void:
+	await LevelTransition.fade_to_black()
+	get_tree().change_scene_to_file("res://level_1.tscn")
+	LevelTransition.fade_from_black()
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
