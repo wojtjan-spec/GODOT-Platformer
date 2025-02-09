5373d64a91ca8e8bdd68572df06e4b4e2bf4d6de
Jan Wojtkowski <janwojtkowski@Jans-MacBook-Pro.local>
2025-02-09 16:38:12 +0100

    Add counter

--- player.gd
+++ player.gd
@@ -33,6 +33,7 @@ func _physics_process(delta: float) -> void:
 	var just_left_wall = was_on_wall and not is_on_wall()
 	if just_left_wall:
 		wall_jump_timer.start()
+	update_animations(input_axis)
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -42,7 +43,7 @@ func handle_jump() -> void:
 	if is_on_floor(): air_jump = true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_pressed("jump"):
 			velocity.y = movement_data.jump_velocity
 	
 	elif not is_on_floor():
@@ -66,7 +67,6 @@ func handle_wall_jump() ->void:
 func apply_friction(input_axis, delta) -> void:
 	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, movement_data.acceleration * delta)
-		
 
 func apply_air_resistance(input_axis, delta) ->void:
 	if input_axis == 0 and not is_on_floor():
--- world.gd
+++ world.gd
@@ -3,10 +3,19 @@ extends Node
 @export var next_level: PackedScene
 
 @onready var level_completed: ColorRect = $CanvasLayer/LevelCompleted
+@onready var start_in: ColorRect = %StartIn
+@onready var start_in_label: Label = %StartInLabel
+@onready var start_animation: AnimationPlayer = $StartAnimation
 
 func _ready():
-	# Assing viusal polygon to the collision polygon
 	Events.level_completed.connect(show_level_completed)
+	get_tree().paused = true
+	start_in.visible = true
+	await LevelTransition.fade_from_black()
+	start_animation.play("countdown")
+	await start_animation.animation_finished
+	get_tree().paused = false
+	start_in.visible = false
 	
 func show_level_completed():
 	level_completed.show()
@@ -16,4 +25,3 @@ func show_level_completed():
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
-	LevelTransition.fade_from_black()
